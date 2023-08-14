32ac2d6e47aaa354785f9489fdcb3e8c8dfd36d6
Ilya Sapronov <ilya@sapronov.net>
2023-08-14 07:33:56 +0300

    create task model, create func for authentication

--- auth/models.py
+++ auth/models.py
@@ -1,7 +1,17 @@
 from hashlib import md5
 from django.db import models
+from cryptography.fernet import Fernet
+
+fernet = Fernet(b'fnEzdP1WtdWv1MtigCnDMHKod-EzumbYM8R2Izz6gyA=')
 
 # Create your models here.
+class UserRole(models.IntegerChoices):
+    ADMIN = 1, "Admin"
+    BOSS = 2, "Boss"
+    DEVELOPER = 3, "Developer"
+    MANAGER = 4, "Manager"
+    ACCOUNTANT = 5, "Accountant"
+
 class User(models.Model):
     ROLE_CHOICES = (
         (1, "Admin"),
@@ -10,28 +20,23 @@ class User(models.Model):
         (4, "Manager"),
         (5, "Accountant"),
     )
-    role = models.PositiveSmallIntegerField("Роль", choices=ROLE_CHOICES)
+    role = models.PositiveSmallIntegerField("Роль", choices=UserRole.choices)
     name = models.CharField("Имя", max_length=50, blank=True, default='')
     email = models.EmailField('Email', max_length=254, unique=True)
     password_md5 = models.CharField("MD5 Пароля", max_length=50, blank=True)
     
     # default id is used as public id
 
-    
-    def set_password(self, new_password):
-        self.password_md5 =
-        self.save()
-
     @staticmethod
-    def create_user(role, email, password, name=''):
+    def create_user(role, email:str, password:str, name:str='') -> 'User':
         user = User.objects.create(role=role, name=name, email=email, password = md5(password).hexdigest())
-        # send CUD event
+        # TODO: send CUD event
         return user
 
     @staticmethod
-    def authenticate(email, password):
+    def get_user_token(email:str, password:str) -> str|None: #authenticate
         user = User.objects.filter(email=email, password= md5(password).hexdigest()).first()
         if not user:
             return
-        
-        
+        return fernet.encrypt(str(user.id).encode()).decode()
+    
--- task_tracker/models.py
+++ task_tracker/models.py
@@ -1,3 +1,60 @@
+import random
 from django.db import models
+from cryptography.fernet import Fernet
+
+fernet = Fernet(b'fnEzdP1WtdWv1MtigCnDMHKod-EzumbYM8R2Izz6gyA=')
 
 # Create your models here.
+
+# Create your models here.
+class UserRole(models.IntegerChoices):
+    ADMIN = 1, "Admin"
+    BOSS = 2, "Boss"
+    DEVELOPER = 3, "Developer"
+    MANAGER = 4, "Manager"
+    ACCOUNTANT = 5, "Accountant"
+
+class User(models.Model):
+    ROLE_CHOICES = (
+        (1, "Admin"),
+        (2, "Boss"),
+        (3, "Developer"),
+        (4, "Manager"),
+        (5, "Accountant"),
+    )
+    role = models.PositiveSmallIntegerField("Роль", choices=UserRole.choices)
+    name = models.CharField("Имя", max_length=50, blank=True, default='')
+    email = models.EmailField('Email', max_length=254, unique=True)
+
+    def get_user_by_token(self, token):
+        return User.objects.get(id=int(fernet.decrypt(token.encode()).decode()))
+
+
+
+class TaskStatus(models.IntegerChoices):
+    IN_PROGRESS = 1, "In Progress"
+    DONE = 2, "Done"
+
+class Task(models.Model):
+    description = models.TextField('Описание задачи')
+    status = models.PositiveSmallIntegerField('Статус задачи', choices=TaskStatus.choices, default=0)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name='Ответственный за выполнение задачи')
+        
+
+    @staticmethod
+    def create_task(description):
+        task = Task.objects.create(description=description)
+        # TODO: send business event and CUD event
+        task.reassign()
+
+    def reassign(self):
+        users = User.objects.exclude(role__in=[UserRole.ADMIN, UserRole.MANAGER])
+        user = random.choice(users)
+        self.user = user
+        self.save(update_fields='user')
+        # TODO: send business event and CUD event
+    
+    def complete(self):
+        self.status = TaskStatus.DONE
+        self.save(update_fields='status')
+        # TODO: send business event and CUD event
